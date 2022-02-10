814dfb7ccd482abf3be1351a4f50d2fe32e082a4
Flavio Rodolfi <flek8rodolfi@gmail.com>
2022-02-10 22:34:49 +0100

    esercizio base

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const vue = new Vue (
+    {
+        el:'#app',
+        data:{
+            items: [
+                {
+                    photo: 'img/01.jpg',
+                    title: "Svezia",
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
+                },
+                {
+                    photo: 'img/02.jpg',
+                    title: "Svizzera",
+                    text: "Lorem ipsum."
+                },
+                {
+                    photo: 'img/03.jpg',
+                    title: "Gran Bretagna",
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+                },
+                {
+                    photo: 'img/04.jpg',
+                    title: "Germania",
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+                },
+                {
+                    photo: 'img/05.jpg',
+                    title: "Paradise",
+                    text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+                }
+            ],
+            active: 0,
+            clock: 0,
+        },
+        methods:{
+
+            prev() {
+
+                if (this.active == 0) {
+                    this.active = this.items.length -1;
+                } else {
+                    this.active--;
+                }
+
+            },
+            next() {
+                
+                if (this.active == this.items.length - 1) {
+                    this.active = 0;
+                } else {
+                    this.active++;
+                }
+            }
+            
+        }       
+    }
+)
